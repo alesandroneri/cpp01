148f49b970b0e6d566c314a8e61e916bd72f950d
aneri-da <alesssandro.soares1604@gmail.com>
2025-10-07 19:00:51 -0300

    feat: zombieHorde.cpp
    - Adding the zombieHorde function

---
+++ ex01/src/zombieHorde.cpp
@@ -0,0 +1,13 @@
+#include "../includes/Zombie.hpp"
+#include <iostream>
+
+Zombie* zombieHorde( int N, std::string name ) {
+    if (N < 1) {
+        std::cout << "Error: the horde can not have less than one zombie." << std::endl;
+        return NULL;
+    }
+    Zombie *horde = new Zombie[N];
+    for (int i = 0; i < N; i++)
+        horde[i].setName(name);
+    return horde;
+}
