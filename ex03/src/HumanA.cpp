b40e16a744e4d7793c2dbed8fd6245ec4ff6cc6b
aneri-da <alesssandro.soares1604@gmail.com>
2025-10-07 19:05:09 -0300

    feat: HumanA.cpp
    - Adding methods of the HumanA class

---
+++ ex03/src/HumanA.cpp
@@ -0,0 +1,15 @@
+#include "../includes/HumanA.hpp"
+#include <iostream>
+
+HumanA::HumanA(std::string name, Weapon weapon) {
+    this->name = name;
+    this->weapon = weapon;
+}
+
+HumanA::~HumanA() {
+    return;
+}
+
+void HumanA::attack(void) {
+    std::cout << this->name << " attacks with their " << this->weapon.getType() << std::endl;
+}
