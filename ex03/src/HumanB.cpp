5d51d8399a39ad6154957c8303946f4925f66d82
aneri-da <alesssandro.soares1604@gmail.com>
2025-10-10 19:08:31 -0300

    fix: HumanB.cpp
    - Changing the constructor of the class HumanB to initialize correctly the has_weapon attribute
    - Changing the attack method to verify if the HumanB is armed before attack
    -Changing the setWeapon method to verify if the HumanB is armed before set a weapon and to change correctly the has weapon attribute

--- ex03/src/HumanB.cpp
+++ ex03/src/HumanB.cpp
@@ -4,16 +4,26 @@
 
 HumanB::HumanB(std::string name) {
     this->name = name;
+    this->has_weapon = false;
 }
 
 HumanB::~HumanB() {
     return;
 }
 
-void HumanB::attack(void) {
-    std::cout << this->name << " attacks with their " << this->weapon.getType() << std::endl;
+void HumanB::attack() {
+    if (has_weapon)
+        std::cout << this->name << " attacks with their " << this->weapon->getType() << std::endl;
+    else
+        std::cout << this->name << " has no weapon and is attacking the zombie with bare hands." << std::endl;
 }
 
-void HumanB::setWeapon(Weapon weapon) {
-    this->weapon = weapon;
+void HumanB::setWeapon(Weapon &weapon) {
+    if (!has_weapon) {
+        this->weapon = &weapon;
+        this->has_weapon = true;
+    }
+    else {
+        this->weapon = &weapon;
+    }
 }
