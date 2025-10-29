bac277ca2480593f34b68ae66492dc2988569e01
aneri-da <alesssandro.soares1604@gmail.com>
2025-10-29 06:19:40 -0300

    fix: main.cpp
    - Changing the main of the ex02 to display correctly the messages in the output

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -7,10 +7,10 @@ int main (void) {
     std::string &stringREF = string;
     
     std::cout << "Adress of str: " << &string << std::endl;
-    std::cout << "Adress of stringPTR: " << &stringPTR << std::endl;
+    std::cout << "Adress of stringPTR: " << stringPTR << std::endl;
     std::cout << "Adress of stringREF: " << &stringREF << std::endl << std::endl;
     std::cout << "Value of str: " << string << std::endl;
-    std::cout << "Value of stringPTR: " << stringPTR << std::endl;
+    std::cout << "Value of stringPTR: " << *stringPTR << std::endl;
     std::cout << "Value of stringREF: " << stringREF << std::endl;
 
     return 0;
